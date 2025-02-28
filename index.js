6727c128c9a6df0d2dbba107aa9675bf37268e69
timothychemweno254 <timothy.kiprop@student.moringaschool.com>
2025-02-28 21:02:49 +0300

    my last la

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(window.getComputedStyle(dodger).left, 10);
+    dodger.style.left = `${left - 4}px`;
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(window.getComputedStyle(dodger).left, 10);
+    dodger.style.left = `${left + 4}px`;
+  }
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
+  
